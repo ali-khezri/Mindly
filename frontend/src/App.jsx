83299c3a842eceee4a4e28e42b9267950b636b57
Ali Khezri <khezri.ali.1994@gmail.com>
2026-09-24 23:14:34 +0330

    CreatePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 const App = () => {
   return (
     <div className="relative h-full w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#943850_100%)]" />
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#f1f1f1_60%,#dd89a0_100%)]" />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
             Mindly
           </h1>
           <div className="flex items-center gap-4">
-            <Link to={"/create"} className="btn btn-primary">
+            <Link to="/create" className="btn btn-primary">
               <PlusIcon className="size-5" />
               <span>New Note</span>
             </Link>
