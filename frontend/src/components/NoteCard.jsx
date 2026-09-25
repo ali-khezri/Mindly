1e3ae056da87547ac057190e4d5b2374f20a2046
Ali Khezri <khezri.ali.1994@gmail.com>
2026-09-25 13:01:32 +0330

    Update UI

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -6,7 +6,7 @@ function NoteCard({ note }) {
   return (
     <Link
       to={`/note/${note._id}`}
-      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#8c0327]"
+      className="card bg-base-200 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#8c0327]"
     >
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
