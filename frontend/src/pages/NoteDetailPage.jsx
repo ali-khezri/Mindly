e5650ef40975929f040418ed261b0f7cb4226fc2
Ali Khezri <khezri.ali.1994@gmail.com>
2026-09-25 21:28:48 +0330

    Note Detail Page

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,5 +1,143 @@
+import { ArrowLeftIcon, Loader, LoaderIcon, Trash2Icon } from "lucide-react";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate, useParams } from "react-router";
+import api from "../lib/axios";
+
 const NoteDetailPage = () => {
-  return <div>NoteDetailPage</div>;
+  const [note, setNote] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSaving, setIsSaving] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function fetchNote() {
+      try {
+        const res = await api.get(`/notes/${id}`);
+
+        setNote(res.data);
+      } catch (error) {
+        console.log("Error in fetching note", error);
+        toast.error("Failed to fetch!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchNote();
+  }, [id]);
+
+  async function handleDelete() {
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+
+      toast.success("Note deleted");
+
+      navigate("/");
+    } catch (error) {
+      console.log("Error deleting the note:", error);
+      toast.error("Failed to delete note");
+    }
+  }
+
+  async function handleSave() {
+    if (!note.title.trim() || !note.content.trim()) {
+      toast.error("Please add a title or content");
+      return;
+    }
+
+    setIsSaving(true);
+
+    try {
+      await api.put(`/notes/${id}`, note);
+
+      toast.success("Note updated successfully!");
+
+      navigate("/");
+    } catch (error) {
+      console.log("Error saving the note:", error);
+      
+      toast.error("Failed to update note");
+    } finally {
+      setIsSaving(false);
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="nim-h-screen bg-base-200">
+      <div className="container mx-auto px-4 py-8">
+        <div className="max-w-2xl mx-auto">
+          <div className="flex items-center justify-between mb-6">
+            <Link to="/" className="btn btn-ghost">
+              <ArrowLeftIcon className="h-5 w-5" />
+              Back to Notes
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="btn btn-error btn-outline"
+            >
+              <Trash2Icon className="h-5 w-5" />
+              Delete Note
+            </button>
+          </div>
+
+          <div className="card bg-base-100">
+            <div className="card-body">
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Note title"
+                  className="input input-bordered"
+                  value={note.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                />
+              </div>
+
+              <div className="form-control mb-4">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea
+                  placeholder="Write your note here..."
+                  className="textarea textarea-bordered h-32"
+                  value={note.content}
+                  onChange={(e) =>
+                    setNote({ ...note, content: e.target.value })
+                  }
+                />
+              </div>
+
+              <div className="card-actions justify-end">
+                <button
+                  className="btn btn-primary"
+                  disabled={isSaving}
+                  onClick={handleSave}
+                >
+                  {isSaving ? "Saving..." : "Save Changes"}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NoteDetailPage;
